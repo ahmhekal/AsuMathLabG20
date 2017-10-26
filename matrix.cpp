8e3e690f0548eb828bbba25ce453fe02cb3f5bc6
AbdallahReda <engabdallahreda@gmail.com>
2017-10-27 01:42:03 +0200

    Definition of functions

--- matrix.cpp
+++ matrix.cpp
@@ -1,69 +1,89 @@
-//class implementation
-//edit
-class CMatrix
-{
-int nR, nC;
-double** values;
-public:
-CMatrix();
-~CMatrix();
-enum MI{MI_ZEROS, MI_ONES, MI_EYE, MI_RAND, MI_VALUE};
-CMatrix(int nR, int nC, int initialization = MI_ZEROS, double
-initializationValue = 0.0);
-CMatrix(int nR, int nC, double first, ...);
-CMatrix(CMatrix& m);
-CMatrix(double d);
-CMatrix(string s);
-void copy(CMatrix& m);
-void copy(double d);
-void copy(string s);
-void reset();
+#include "Matrix.h"
 
-string getString();
-CMatrix operator=(CMatrix& m);
-CMatrix operator=(double d);
-CMatrix operator=(string s);
-void add(CMatrix& m);
-void operator+=(CMatrix& m);
-void operator+=(double d);
-CMatrix operator+(CMatrix& m);
-CMatrix operator+(double d);
-void sub(CMatrix& m);
-void operator-=(CMatrix& m);
-void operator-=(double d);
-CMatrix operator-(CMatrix& m);
-CMatrix operator-(double d);
-void mul(CMatrix& m);
-void operator*=(CMatrix& m);
-void operator*=(double d);
-CMatrix operator*(CMatrix& m);
-CMatrix operator*(double d);
-void div(CMatrix& m);
-void operator/=(CMatrix& m);
-void operator/=(double d);
-CMatrix operator/(CMatrix& m);
-CMatrix operator/(double d);
-CMatrix operator++(); //Pre Increment
-CMatrix operator++(int); //Post Increment, int is not used
-CMatrix operator--(); //Pre Increment
-CMatrix operator--(int); //Post Increment, int is not used
+//**************************************Branch [ahmhekal]**************************************//
 
-CMatrix operator-();
-CMatrix operator+();
-friend istream& operator >> (istream &is, CMatrix& C); //Stream
-friend ostream& operator << (ostream &os, CMatrix& C); //Stream
-void setSubMatrix(int iR, int iC, CMatrix& m);
-CMatrix getSubMatrix(int r, int c, int nr, int nc);
-CMatrix getCofactor(int r, int c);
-void addColumn(CMatrix& m);
-void addRow(CMatrix& m);
-double& operator[](int i){return values[i/nC][i%nC];}
-double& operator()(int i){return values[i/nC][i%nC];}
-double& operator()(int r, int c){return values[r][c];}
-int getn(){return nR*nC;};
-int getnR(){return nR;};
-int getnC(){return nC;};
-double getDeterminant();
-double getTranspose();
-double getInverse();
-};
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
+
+//**************************************Branch [diaaahmed850]**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
+ 
+
+
+//**************************************Block 3**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+//**************************************Block 4**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
+//**************************************Branch [Abduu13]**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
+
+//**************************************Branch [AbdallahReda]**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
+
+//**************************************Branch [Aladdin95]**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
+//**************************************Branch [omarashraf10]**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
+//**************************************Branch [noureddin]**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
+//**************************************Branch [mafouadm]**************************************//
+
+ //---------------------------------write your code here---------------------------//
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+
