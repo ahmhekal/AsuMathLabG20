15598c746db57d121ac63d33a28c650213fcd7f1
diaaahmed850 <diaaahmed850@gmail.com>
2018-01-22 01:17:58 +0200

    supporting sqrt

--- parser2.cpp
+++ parser2.cpp
@@ -60,4 +60,23 @@ a.replace(first_pos,end_pos-first_pos+1,(string)buffer_test);
 delete [] buffer;
 
 }
+if(a.find("sqrt")!=string::npos){
+int first_pos=a.find("sqrt");
+int end_pos=a.find(')',first_pos);
+string expression=a.substr(first_pos,end_pos-first_pos+1);
+string operand_str=expression.substr(5,a.length()-5-1);
+char * buffer=new char[operand_str.length()+1];
+strcpy(buffer,operand_str.c_str());
+double operand=atof(buffer);
+double result=sqrt(operand);
+char buffer_test[50];
+sprintf(buffer_test,"%g",result);
+a.replace(first_pos,end_pos-first_pos+1,(string)buffer_test);
+
+delete [] buffer;
+
+
+
+}
+
 }
