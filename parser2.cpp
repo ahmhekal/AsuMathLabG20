e752d97d76a7e1af5be4b6426be0307b12a53f4f
diaaahmed850 <diaaahmed850@gmail.com>
2018-01-22 01:12:11 +0200

    supportin tan and cos

--- parser2.cpp
+++ parser2.cpp
@@ -25,4 +25,39 @@ a.replace(first_pos,end_pos-first_pos+1,(string)buffer_test);
 delete [] buffer;
 
 
-}}
+}
+if(a.find("cos")!=string::npos){
+int first_pos=a.find("cos");
+int end_pos=a.find(')',first_pos);
+string expression=a.substr(first_pos,end_pos-first_pos+1);
+string operand_str=expression.substr(4,a.length()-4-1);
+char * buffer=new char[operand_str.length()+1];
+strcpy(buffer,operand_str.c_str());
+double operand=atof(buffer);
+double result=cos(operand);
+char buffer_test[50];
+sprintf(buffer_test,"%g",result);
+a.replace(first_pos,end_pos-first_pos+1,(string)buffer_test);
+
+delete [] buffer;
+
+
+
+}
+ if(a.find("tan")!=string::npos){
+int first_pos=a.find("tan");
+int end_pos=a.find(')',first_pos);
+string expression=a.substr(first_pos,end_pos-first_pos+1);
+string operand_str=expression.substr(4,a.length()-4-1);
+char * buffer=new char[operand_str.length()+1];
+strcpy(buffer,operand_str.c_str());
+double operand=atof(buffer);
+double result=tan(operand);
+char buffer_test[50];
+sprintf(buffer_test,"%g",result);
+a.replace(first_pos,end_pos-first_pos+1,(string)buffer_test);
+
+delete [] buffer;
+
+}
+}
