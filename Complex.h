26f704aee3e31c587b3f4817c4fedf8153dea314
AbdallahReda <30808065+AbdallahReda@users.noreply.github.com>
2018-01-27 23:04:04 +0200

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -1,30 +1,52 @@
 #include <math.h>
 #include <iostream>
+#include <cmath>
+#include <math.h>
+
 using namespace std;
 
 class Complex{
-	float R; float I;
+	double R; double I;
 public:
 	Complex() {R=0; I=0;}
-	Complex(float r, float i){R=r;I=i;}
-	void SetComplex(float r , float i);
-	float GetReal();
-	float GetImaj();
-	void SetReal(float r);
-	void SetImaj(float i);
-	Complex Add(Complex x);
-	Complex Subtract(Complex x);
-	Complex Multiply(Complex x);
-	Complex Divide(Complex x);
+	Complex(double r, double i){R=r;I=i;}
+
+	void SetComplex(double r , double i);
+
+	double GetR();
+	double GetIm();
+
+	void SetR(double r);
+	void SetIm(double i);
+
+	void Add(Complex& x);
+	void Subtract(Complex& x);
+	void Multiply(Complex& x);
+	void Divide(Complex& x);
+
 	Complex Conjugate();
-	Complex Power(Complex x, int y);
-	Complex operator =(Complex x);
-	Complex operator +(Complex x);
-	Complex operator -(Complex x);
-	Complex operator *(Complex x);
-	Complex operator /(Complex x);
-	Complex operator *=(Complex x); 
+	Complex Power(Complex& x, int y);
+	double modulus();
+
+	Complex AddComplex(Complex& x,Complex& y);
+	Complex SubtractComplex(Complex& x,Complex& y);
+	Complex MultiplyComplex(Complex& x);
+	Complex DivideComplex(Complex& x);
+
+	Complex operator =(Complex& x);
+	Complex operator +(Complex& x);
+	Complex operator -(Complex& x);
+	Complex operator *(Complex& x);
+	Complex operator /(Complex& x);
+
+
+	void operator +=(Complex& x);
+	void operator -=(Complex& x);
+	void operator *=(Complex& x);
+	void operator /=(Complex& x);
+
 	Complex operator^(int n);
-	friend ostream&  operator << ( ostream& o , Complex c);
+
+	friend ostream&  operator << ( ostream& o ,Complex& c);
 
 };
