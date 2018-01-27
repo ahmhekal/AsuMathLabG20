7eee1d727f23e43b0409f7f31e315f51c34f6122
AbdallahReda <30808065+AbdallahReda@users.noreply.github.com>
2018-01-27 23:04:59 +0200

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,42 +1,82 @@
 #include "Complex.h"
 
 
-void Complex :: SetComplex(float r , float i){R=r; I=i;}
-float Complex :: GetReal(){ return R;}
-float Complex :: GetImaj(){ return I;}
-void Complex :: SetReal(float r){ R=r;}
-void Complex :: SetImaj(float i){ I=i;}
-Complex Complex :: Add(Complex x){ Complex c; c.R= R + x.R; c.I= I + x.I;  return c; }
-Complex Complex :: Subtract(Complex x){ Complex c; c.R= R - x.R; c.I= I - x.I;  return c; }
-Complex Complex :: Multiply(Complex x){ Complex c;  c.R=(R*x.R)-(I*x.I);  c.I=(R*x.I)+(I*x.R);  return c; }
+void Complex :: SetComplex(double r , double i){R=r; I=i;}
 
-Complex Complex :: Divide(Complex x){ 
-	int div=(x.R*x.R) + (x.I*x.I);
+double Complex :: GetR(){ return R;}
+double Complex :: GetIm(){ return I;}
+
+void Complex :: SetR(double r){ R=r;}
+void Complex :: SetIm(double i){ I=i;}
+
+
+void Complex :: Add(Complex& x){R += x.R; I += x.I; }
+
+void Complex :: Subtract(Complex& x){R -= x.R; I -= x.I;}
+
+void Complex :: Multiply(Complex& x){double Real=R; double Imaj=I;  R=(Real*x.R)-(Imaj*x.I); I=(Real*x.I)+(Imaj*x.R);}
+
+void Complex :: Divide(Complex& x){ 
+	double Real=R; double Imaj=I;
+	R=((Real*x.R)+(Imaj*x.I))/((x.R*x.R) + (x.I*x.I)); I=((Imaj*x.R)-(Real*x.I))/((x.R*x.R) + (x.I*x.I));
+}
+
+
+Complex Complex ::AddComplex(Complex& x,Complex& y){Complex C;C.R = x.R + y.R; C.I = x.I + y.I; return C;}
+
+Complex Complex ::SubtractComplex(Complex& x,Complex& y){Complex C;C.R = x.R - y.R; C.I = x.I - y.I; return C;}
+
+Complex Complex ::MultiplyComplex(Complex& x){Complex c;  c.R=(R*x.R)-(I*x.I);  c.I=(R*x.I)+(I*x.R);  return c;}
+
+Complex Complex ::DivideComplex(Complex& x){
+	double div=(x.R*x.R) + (x.I*x.I);
 	Complex tmp; 
 	tmp.R=(R*x.R)+(I*x.I); tmp.R/=div; tmp.I=(I*x.R)-(R*x.I); tmp.I/=div; 
-	return tmp; }
-
-ostream&  operator << ( ostream& o , Complex c)
-{
-	if(c.GetReal()>0 && c.GetImaj()>0){o<<c.GetReal()<<" + "<<c.GetImaj()<<"i"<<"\n";return o;}
-	if(c.GetReal()>0 && c.GetImaj()<0){o<<c.GetReal()<<" - "<<-c.GetImaj()<<"i"<<"\n";return o;}
-	if(c.GetReal()>0 && c.GetImaj()==0){o<<c.GetReal()<<"\n"; return o;}
-	if(c.GetReal()==0 && c.GetImaj()>0){o<<c.GetImaj()<<"i"<<"\n"; return o;}
-	if(c.GetReal()==0 && c.GetImaj()<0){o<<" - "<<-c.GetImaj()<<"i"<<"\n";return o;}
-	if(c.GetReal()==0 && c.GetImaj()==0){o<<c.GetReal()<<"\n"; return o;}
-	if(c.GetReal()<0 && c.GetImaj()<0){o<<c.GetReal()<<" - "<<-c.GetImaj()<<"i"<<"\n";return o;} // look at "-c.GetImaj()"
-	if(c.GetReal()<0 && c.GetImaj()>0){o<<c.GetReal()<<" + "<<c.GetImaj()<<"i"<<"\n";return o;}
-	if(c.GetReal()<0 && c.GetImaj()==0){o<<c.GetReal()<<"\n";return o;}
+	return tmp;
 }
-Complex Complex:: operator = (Complex x ) {R=x.GetReal(); I=x.GetImaj(); return x;}
-Complex Complex:: operator + (Complex x ) {return Add(x);}
-Complex Complex:: operator - (Complex x ) {return Subtract(x);}
-Complex Complex:: operator * (Complex x ) {return Multiply(x);}
-Complex Complex:: operator / (Complex x ) {return Divide(x);}
-Complex Complex:: operator *=(Complex x ) {Complex temp; temp.SetComplex(1,1);return Multiply(x);}
-Complex Complex:: Conjugate() {Complex tmp;tmp.R=this->R;tmp.I=this->I*(-1); return tmp;}
-Complex Complex:: operator^(int n){
+
+Complex Complex::Power(Complex& input, int power)
+{
 	Complex ONE=Complex(1,0);
-	if (n<=0) return ONE;
-	return ((*this)*((*this)^(n-1)));
+    if (power>= 0) {for (int i = 0; i < power; ++i) ONE = ONE * input; }
+    else		   {for (int i = 0; i > power; --i) ONE = ONE / input; }
+    return ONE;
+}
+
+double Complex :: modulus() {double absolute = sqrt((R*R)+(I*I)); return absolute; }
+
+Complex Complex:: Conjugate() {Complex tmp;tmp.R=this->R;tmp.I=this->I*(-1); return tmp;}
+
+
+Complex Complex:: operator = ( Complex& x ) {R=x.GetR(); I=x.GetIm(); return x;}
+Complex Complex:: operator + ( Complex& x ) {return AddComplex(*this,x);}
+Complex Complex:: operator - ( Complex& x ) {return SubtractComplex(*this,x);}
+Complex Complex:: operator * ( Complex& x ) {return MultiplyComplex(x);}
+Complex Complex:: operator / ( Complex& x ) {return DivideComplex(x);}
+
+void Complex:: operator +=(Complex& x ) { Add(x);}
+void Complex:: operator -=(Complex& x ) { Subtract(x);}
+void Complex:: operator /=(Complex& x ) { Divide(x);}
+void Complex:: operator *=(Complex& x ) { Multiply(x);}
+
+
+
+Complex Complex:: operator^(int n){
+	return Power(*this,n);
+}
+
+
+
+ostream&  operator << ( ostream& o , Complex& c)
+{
+	if(c.GetR()>0 && c.GetIm()>0){o<<c.GetR()<<" + "<<c.GetIm()<<"i"<<"\n";return o;}
+	else if(c.GetR()>0 && c.GetIm()<0){o<<c.GetR()<<" - "<<-c.GetIm()<<"i"<<"\n";return o;}
+	else if(c.GetR()>0 && c.GetIm()==0){o<<c.GetR()<<"\n"; return o;}
+	else if(c.GetR()==0 && c.GetIm()>0){o<<c.GetIm()<<"i"<<"\n"; return o;}
+	else if(c.GetR()==0 && c.GetIm()<0){o<<" - "<<-c.GetIm()<<"i"<<"\n";return o;}
+	else if(c.GetR()==0 && c.GetIm()==0){o<<c.GetR()<<"\n"; return o;}
+	else if(c.GetR()<0 && c.GetIm()<0){o<<c.GetR()<<" - "<<-c.GetIm()<<"i"<<"\n";return o;} // look at "-c.GetIm()"
+	else if(c.GetR()<0 && c.GetIm()>0){o<<c.GetR()<<" + "<<c.GetIm()<<"i"<<"\n";return o;}
+	//else if(c.GetR()<0 && c.GetIm()==0){o<<c.GetR()<<"\n";return o;}
+	else {o<<c.GetR()<<"\n";return o;}
 }
