fc60f100655dab1c8a2c1c4c2c4efaf26c0d58c4
mohamed fouad <33027971+mafouadm@users.noreply.github.com>
2018-01-27 23:45:00 +0200

    Update functions.h
    hypot modified using double

--- functions.h
+++ functions.h
@@ -577,3 +577,12 @@ asu::CMatrix atan2d_modified(const asu::CMatrix& a, double m )
 		r(i) = atan2d(a(i), m);
 	return r;
 }
+//hypot modified using double
+asu::CMatrix hypot_modified(const asu::CMatrix& a,double m )
+{
+	
+	asu::CMatrix r(a.getnRows(), a.getnColumns());
+	for (size_t i = 0; i < a.getn(); i++)
+		r(i) = hypot(a(i), m);
+	return r;
+}
